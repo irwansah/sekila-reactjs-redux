91ec203f0cdac21cc17ea8cb12026533616635f2
irwansah <irwansah607@gmail.com>
2021-08-12 09:48:09 +0700

    update

--- src/reducers/users.js
+++ src/reducers/users.js
@@ -1,30 +1,7 @@
 
   let initialState = {
     title:"Sekila",
-    users:[
-        {
-         id:1,
-         nama:"irwan",
-         alamat:"cianjur",
-         umur:"23",
-         nohp:"085814429029"
-        },
-        {
-         id:2,
-         nama:"arwin",
-         alamat:"cianjur",
-         umur:"21",
-         nohp:"085814429029"
-        },
-        {
-         id:3,
-         nama:"irma",
-         alamat:"cianjur",
-         umur:"22",
-         nohp:"085814429029"
-        },
-
-      ],
+    users:false,
       error:false
   };
   
